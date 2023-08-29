196aaf5a450fcb7b8518faac599708fb7965eea8
anchal ghale <anchalghale@gmail.com>
2023-08-29 16:46:12 +0545

    chore: release 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ucaptcha",
-    version="1.0.1",
+    version="1.0.2",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description="Universal captcha solving python module",
